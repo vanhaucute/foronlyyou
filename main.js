b894c5e0eb563a2441503fb3018ff355d63fce35
Cuder <87767423+vanhaucute@users.noreply.github.com>
2021-10-08 22:01:47 +0700

    update

--- main.js
+++ main.js
@@ -1,48 +1,47 @@
-var mainContainer = document.querySelector(".main-container")
-var agree = document.querySelector(".agree")
-var notAgreeBtn = document.querySelector(".notAgree")
-var agreeBtn = document.querySelector(".agree")
-var startBtn = document.querySelector(".start")
+var mainBox = document.querySelector(".main-box")
+var okayBtn = document.querySelector(".okay")
+var noBtn = document.querySelector(".no")
+
+var startBtn = document.querySelector(".js-start")
 var screens = document.querySelectorAll(".screen")
 
 function getRandomPosition() {
-     var x = Math.floor(Math.random() * mainContainer.clientWidth)
-     var y = Math.floor(Math.random() * mainContainer.clientHeight) + 47
+     var x = Math.floor(Math.random() * mainBox.clientWidth)
+     var y = Math.floor(Math.random() * mainBox.clientHeight) 
 
-     x = x + notAgreeBtn.offsetWidth > mainContainer.clientWidth ? x - notAgreeBtn.offsetWidth : x 
-     y = y + notAgreeBtn.offsetHeight > mainContainer.clientHeight ? y - notAgreeBtn.offsetWidth : y 
+     x = x + noBtn.offsetWidth > mainBox.clientWidth ? x - noBtn.offsetWidth : x 
+     y = y + noBtn.offsetHeight > mainBox.clientHeight ? y - noBtn.offsetWidth : y 
 
      return { x , y }
 }
 
+startBtn.addEventListener("click" , () => screens[1].classList.add("up"))
 
+
+var number = 0
 function move() {
      var { x , y } = getRandomPosition()
 
-     notAgreeBtn.style.left = x + "px"
-     notAgreeBtn.style.top  = y + "px"
+     noBtn.style.left = x + "px"
+     noBtn.style.top  = y + "px"
      number++
+}
+
+function disableBtn() {
      if (number === 7) {
-          // notAgreeBtn.setAttribute("disabled" , "")
-          notAgreeBtn.removeEventListener("click" , move , false)
+          noBtn.removeEventListener("click" , move , false)
      }
-     
 }
+disableBtn()
 
-function startMove() {
-     setTimeout(move , 3500)
-}
-
-notAgreeBtn.addEventListener("click" , move)
-agreeBtn.addEventListener("click" , () => screens[1].classList.add("up"))
-startBtn.addEventListener("click" , () => screens[0].classList.add("up"))
+noBtn.addEventListener("click" , move)
+okayBtn.addEventListener("click" , () => screens[2].classList.add("up"))
 
 
-var text = "Tại vì cậu rất là đẹp trai í<3"
-var send = document.querySelector(".send")
+var text = "Tại vì cậu rất là đẹp trai í<3 hihi"
+var send = document.querySelector(".js-send")
 var inputBox = document.querySelector(".input")
 var myIndex = 0
-var number = 0
 
 
 function write() {
@@ -56,36 +55,10 @@ function write() {
 send.addEventListener("click" , function(e) {
      if (myIndex < 26) {
           e.preventDefault()
+     } else {
+          screens[3].classList.add("up")
      }
 })
 
+
 inputBox.addEventListener("input" , write)
-
-
-
-// Button Ripple Effect
-var allBtns = document.querySelectorAll(".btn")
-
-allBtns.forEach(btn => {
-     btn.addEventListener("click" ,function(e) {
-
-          var x = e.clientX
-          var y = e.clientY
-                
-          var buttonTop = e.target.offsetTop
-          var buttonLeft = e.target.offsetLeft
-
-          var xInside = x - buttonLeft
-          var yInside = y - buttonTop
-
-          var circle = document.createElement("span")
-          circle.classList.add("circle")
-          circle.style.top = yInside + "px"
-          circle.style.left = xInside + "px"
-                
-          this.appendChild(circle)
-
-          setTimeout(() => this.removeChild(circle) , 500)
-
-     })
-})
